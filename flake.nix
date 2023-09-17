ce4f4ed80db2e36189627331cc55cba8e3e6b372
James Wilmot <jameswilmot2000@gmail.com>
2023-09-17 14:41:57 +1000

    dev env: added .envrc and flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "xelatex flake";
+
+  # Use the unstable nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.ruby
+        ];
+      };
+    });
+}
