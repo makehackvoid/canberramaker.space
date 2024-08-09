4cac0ea3123db99490c34c34718fe32396391e87
JamesWilmot <JamesWilmot@users.noreply.github.com>
2024-08-09 09:52:40 +0000

    jekyll build from Action 973eebd768beb7968f58b32416bd07a1a813e43c

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "canberramaker.space website flake";
+
+  # Use the unstable nixpkgs to use the latest set of node packages
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.ruby
+        ];
+      };
+    });
+}
