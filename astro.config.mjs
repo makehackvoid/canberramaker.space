b7e9ec5d481948f298a088ee7df91fcfcc8c73da
Ingo Wolf <me@ingo.au>
2026-04-10 12:59:26 +1000

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-template.netlify.app",
+  site: "https://canberramaker.space",
   integrations: [mdx(), svelte(), solidJs()],
   markdown: {
     shikiConfig: {
